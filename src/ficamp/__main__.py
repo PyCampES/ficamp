d0f31f3a6a045c323d005c9e79ea4aadbe0f82c2
Toni Grabulosa <tonigrabulosa.dev@gmail.com>
2024-03-30 23:32:06 +0100

    adapt bbva and bsabadell parsers

--- src/ficamp/__main__.py
+++ src/ficamp/__main__.py
@@ -3,6 +3,10 @@ import argparse
 from dotenv import load_dotenv
 
 from ficamp.parsers.abn import AbnParser
+from ficamp.parsers.bsabadell import AccountBSabadellParser
+from ficamp.parsers.bsabadell import CreditCardBSabadellParser
+from ficamp.parsers.bbva import AccountBBVAParser
+from ficamp.parsers.bbva import CreditCardBBVAParser
 
 
 def cli() -> argparse.Namespace:
@@ -13,7 +17,9 @@ def cli() -> argparse.Namespace:
         prog="ficamp", description="Print the first argument from the CLI"
     )
 
-    parser.add_argument("--bank", choices=["abn"], default="abn")
+    parser.add_argument("--bank", choices=[
+        "abn", "bbva-account", "bbva-credit-card", "bsabadell-account", "bsabadell-credit-card"
+    ], default="abn")
     parser.add_argument("filename", help="The spreadsheet to load")
 
     # Parse the arguments
@@ -35,6 +41,30 @@ def main():
         transactions = parser.parse()
         print(transactions)
         # TODO: Add categorizer!
+
+    if args.bank == "bsabadell-account":
+        parser = AccountBSabadellParser()
+        parser.load(args.filename)
+        transactions = parser.parse()
+        print(transactions)
+
+    if args.bank == "bsabadell-credit-card":
+        parser = CreditCardBSabadellParser()
+        parser.load(args.filename)
+        transactions = parser.parse()
+        print(transactions)
+
+    if args.bank == "bbva-account":
+        parser = AccountBBVAParser()
+        parser.load(args.filename)
+        transactions = parser.parse()
+        print(transactions)
+
+    if args.bank == "bbva-credit-card":
+        parser = CreditCardBBVAParser()
+        parser.load(args.filename)
+        transactions = parser.parse()
+        print(transactions)
 
 
 load_dotenv()
